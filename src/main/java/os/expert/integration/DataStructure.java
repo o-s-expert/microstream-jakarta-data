9666b4ad12dd40c26203600dc8e4df4b63a5fbf7
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-02 18:37:37 +0000

    feat: Define datastructure to a map inside
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/integration/DataStructure.java
+++ src/main/java/os/expert/integration/DataStructure.java
@@ -10,6 +10,11 @@ public class DataStructure {
     private final Map<Object, Object> data = new LazyHashMap();
 
 
+    public  <K, V> void put(K key, V value) {
+       Objects.requireNonNull(key, "key is required");
+        Objects.requireNonNull(value, "value is required");
+        this.data.put(key, value);
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) {
