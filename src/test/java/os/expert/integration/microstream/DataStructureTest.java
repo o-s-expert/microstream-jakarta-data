cb8e30b6b7423e21dbc2c424acecc87699f8980e
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-02 18:59:46 +0000

    test: create DataStructureTest
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/test/java/os/expert/integration/microstream/DataStructureTest.java
@@ -0,0 +1,16 @@
+package os.expert.integration.microstream;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataStructureTest {
+
+    private DataStructure data;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+}
