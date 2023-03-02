ac6f040b17098f24953d82c7595dc532ae8d7f45
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-02 18:37:56 +0000

    feat: define storage manager as supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/integration/StorageManagerSupplier.java
+++ src/main/java/os/expert/integration/StorageManagerSupplier.java
@@ -1,2 +1,23 @@
-package os.expert.integration;public class StorageManagerSupplier {
+package os.expert.integration;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import one.microstream.persistence.binary.one.microstream.collections.lazy.BinaryHandlerLazyHashMap;
+import one.microstream.storage.embedded.configuration.types.EmbeddedStorageConfiguration;
+import one.microstream.storage.embedded.types.EmbeddedStorageFoundation;
+import one.microstream.storage.types.StorageManager;
+
+import java.util.function.Supplier;
+
+@ApplicationScoped
+public class StorageManagerSupplier implements Supplier<StorageManager> {
+
+    @Override
+    public StorageManager get() {
+        EmbeddedStorageFoundation<?> storageFoundation = EmbeddedStorageConfiguration.Builder()
+                .setStorageDirectory("target/data").createEmbeddedStorageFoundation();
+        storageFoundation.registerTypeHandler(new BinaryHandlerLazyHashMap());
+
+        return storageFoundation.createEmbeddedStorageManager();
+    }
 }
