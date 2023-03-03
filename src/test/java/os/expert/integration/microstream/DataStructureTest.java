1d391872e774fe5aefad15293c5411ccec1f86b1
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-03 11:09:16 +0000

    test: create data structure scenarios test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/DataStructureTest.java
+++ src/test/java/os/expert/integration/microstream/DataStructureTest.java
@@ -1,6 +1,11 @@
 package os.expert.integration.microstream;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,17 +18,81 @@ class DataStructureTest {
         this.data = new DataStructure();
     }
 
-    public void shouldPut() {}
+    @Test
+    public void shouldPut() {
+        this.data.put("one", 1);
+        this.data.put("two", 2);
+        this.data.put("four", 4);
 
-    public void shouldGet() {}
+        Assertions.assertThat(this.data)
+                .isNotNull()
+                .matches(p -> p.size() == 3);
 
-    public void shouldRemove() {}
+    }
 
-    public void shouldSize() {}
+    @Test
+    public void shouldGet() {
+        this.data.put("one", 1);
+        Optional<Object> one = this.data.get("one");
+        Assertions.assertThat(one)
+                .isPresent()
+                .get()
+                .isEqualTo(1);
 
-    public void shouldIsEmpty() {}
+        Optional<Object> two = this.data.get("two");
+        Assertions.assertThat(two)
+                .isNotPresent();
+    }
 
-    public void shouldValue() {}
+    @Test
+    public void shouldRemove() {
+        this.data.put("one", 1);
+        Optional<Object> one = this.data.get("one");
+        Assertions.assertThat(one)
+                .isPresent()
+                .get()
+                .isEqualTo(1);
+
+        this.data.remove("one");
+
+        Assertions.assertThat(this.data.get("one"))
+                .isNotPresent();
+    }
+
+    @Test
+    public void shouldSize() {
+        this.data.put("one", 1);
+        this.data.put("two", 2);
+        this.data.put("four", 4);
+
+        Assertions.assertThat(this.data.size())
+                .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldIsEmpty() {
+        Assertions.assertThat(this.data.isEmpty())
+                .isTrue();
+
+        this.data.put("one", 1);
+        this.data.put("two", 2);
+        this.data.put("four", 4);
+
+        Assertions.assertThat(this.data.isEmpty())
+                .isFalse();
+
+    }
+
+    @Test
+    public void shouldValue() {
+        this.data.put("one", 1);
+        this.data.put("two", 2);
+        this.data.put("four", 4);
+
+        Assertions.assertThat(this.data.values())
+                .hasSize(3)
+                .contains(1, 2, 4);
+    }
 
 
 }
