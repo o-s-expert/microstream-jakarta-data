2c1577deff90e2fd7dad9d69f21e49f3b905f6ca
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-03 11:32:30 +0000

    feat: create get method on field metadata
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/integration/microstream/FieldMetadata.java
+++ src/main/java/os/expert/integration/microstream/FieldMetadata.java
@@ -1,8 +1,18 @@
 package os.expert.integration.microstream;
 
+import jakarta.data.exceptions.MappingException;
+
 import java.lang.reflect.Field;
 
 record FieldMetadata (Field field, String name){
 
+    <T> Object get(T entity) {
+        try {
+            return this.field.get(entity);
+        } catch (IllegalAccessException e) {
+            throw new MappingException("It cannot access the value from the field " + field + " at the entity "
+            + entity.getClass());
+        }
+    }
 
 }
