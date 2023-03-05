e4433609e8a48acb128f8372d9fcf4b75fd53ea9
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-05 21:23:16 +0000

    test: create test for entity metadata
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/EntityMetadataTest.java
+++ src/test/java/os/expert/integration/microstream/EntityMetadataTest.java
@@ -1,6 +1,27 @@
 package os.expert.integration.microstream;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class EntityMetadataTest {
-  
+
+
+    @Test
+    public void shouldReturnErrorWhenIsNull() {
+        assertThrows(NullPointerException.class, () -> EntityMetadata.of(null));
+    }
+
+    @Test
+    public void shouldCreateEntityMetadata() {
+        EntityMetadata metadata = EntityMetadata.of(Person.class);
+
+        assertThat(metadata)
+                .isNotNull()
+                .matches(m -> m.type().equals(Person.class))
+                .matches(m -> m.id().name().equals("id"))
+                .extracting(m -> m.fields());
+    }
 }
