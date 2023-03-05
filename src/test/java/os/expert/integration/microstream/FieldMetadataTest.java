645a1c6c3f9a6c73f368cb2dbe382096caf8383a
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-05 21:55:00 +0000

    test: Create Person structre
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/FieldMetadataTest.java
+++ src/test/java/os/expert/integration/microstream/FieldMetadataTest.java
@@ -1,8 +1,15 @@
 package os.expert.integration.microstream;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FieldMetadataTest {
 
 
+    @Test
+    public void shouldCreateEntityMetadata() {
+        EntityMetadata metadata = EntityMetadata.of(Person.class);
+
+    }
 }
--- src/test/java/os/expert/integration/microstream/Person.java
+++ src/test/java/os/expert/integration/microstream/Person.java
@@ -11,10 +11,32 @@ public class Person {
     @Id
     private String id;
 
-
     @Column
     private String name;
 
     @Column
     private LocalDate birthday;
+
+
+    private Person(String id, String name, LocalDate birthday) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public LocalDate birthday() {
+        return birthday;
+    }
+
+    public Person of(String id, String name, LocalDate birthday) {
+        return new Person(id, name, birthday);
+    }
 }
