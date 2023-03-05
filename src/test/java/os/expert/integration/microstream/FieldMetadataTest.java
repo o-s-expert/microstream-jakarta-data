1ca202e9bc6b05c452f5b0cafaef2101c178fa28
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-05 22:24:19 +0000

    test: create scenario to Column annotatoion
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/FieldMetadataTest.java
+++ src/test/java/os/expert/integration/microstream/FieldMetadataTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +12,7 @@ class FieldMetadataTest {
 
 
     @Test
-    public void shouldCreateEntityMetadata() {
+    public void shouldGetFromId() {
         EntityMetadata metadata = EntityMetadata.of(Person.class);
 
         Person poliana = Person.of("poliana", "Poliana", LocalDate.now());
@@ -19,5 +20,19 @@ class FieldMetadataTest {
 
         Object value = id.get(poliana);
         Assertions.assertEquals("poliana", value);
+    }
+
+    @Test
+    public void shouldGetFromColumn() {
+        EntityMetadata metadata = EntityMetadata.of(Person.class);
+
+        Person poliana = Person.of("poliana", "Poliana", LocalDate.now());
+        List<FieldMetadata> fields = metadata.fields();
+
+        FieldMetadata field = fields.get(0);
+        Object value = field.get(poliana);
+
+        Assertions.assertEquals("Poliana", value);
+
     }
 }
