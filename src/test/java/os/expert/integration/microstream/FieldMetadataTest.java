fe47601e3815af471e47441017b8e4ac482695a9
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-06 19:59:51 +0000

    test: define metadata
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/FieldMetadataTest.java
+++ src/test/java/os/expert/integration/microstream/FieldMetadataTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,6 +35,21 @@ class FieldMetadataTest {
         Object value = field.get(poliana);
 
         Assertions.assertEquals("Poliana", value);
+
+    }
+
+    @Test
+    public void shouldOrderByField() {
+        EntityMetadata metadata = EntityMetadata.of(Person.class);
+        Person poliana = Person.of("poliana", "Poliana", LocalDate.now());
+        Person otavio = Person.of("otavio", "Poliana", LocalDate.now());
+        Person ada = Person.of("ada", "Poliana", LocalDate.now());
+        FieldMetadata id = metadata.id();
+        List<Person> people = List.of(poliana, otavio, ada);
+        Comparator<Person> comparator =  id.comparator();
+
+        Function<Person, Comparable> id1 = Person::id;
+        Comparator.comparing(id1);
 
     }
 }
