19be777ae4a9a722ffbe50c121fee481fc0ed037
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-07 13:48:14 +0000

    test: define Class scanner test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/ClassScannerTest.java
+++ src/test/java/os/expert/integration/microstream/ClassScannerTest.java
@@ -1,7 +1,27 @@
 package os.expert.integration.microstream;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassScannerTest {
+
+    private ClassScanner scanner;
+
+    @BeforeEach
+    public void setUp() {
+        this.scanner = ClassScanner.INSTANCE;
+    }
+
+    @Test
+    public void shouldReturnEntity() {
+        Set<Class<?>> entities = this.scanner.entities();
+        Assertions.assertThat(entities).hasSize(1)
+                .contains(Book.class);
+    }
 
 }
