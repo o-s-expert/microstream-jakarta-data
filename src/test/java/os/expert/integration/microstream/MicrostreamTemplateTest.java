f3d8d5cecc247118c2a738dd2715461e8eaad60f
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-08 09:43:00 +0000

    test: define skeleton to MicrostreamTemplae
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/MicrostreamTemplateTest.java
+++ src/test/java/os/expert/integration/microstream/MicrostreamTemplateTest.java
@@ -1,6 +1,21 @@
 package os.expert.integration.microstream;
 
+import jakarta.nosql.Template;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MicrostreamTemplateTest {
-  
+
+    private DataStructure data;
+
+    private EntityMetadata metadata;
+
+    private Template template;
+
+    @Test
+    public void setUp() {
+
+    }
+
 }
