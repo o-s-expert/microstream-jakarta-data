f8320e5e18bb3f1d5ad0837d8e535fec0818cabb
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-08 09:30:40 +0000

    doc: create package-info
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/os/expert/integration/microstream/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package provides classes for integrating Microstream with two Jakarta specifications for persistence layers:
+ * Jakarta Data and Jakarta NoSQL.
+ */
+package os.expert.integration.microstream;
