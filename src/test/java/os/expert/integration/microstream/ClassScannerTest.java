9145e3eb93a8af68e637c19c2d53f5ff33ea26e1
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-08 09:46:41 +0000

    refac: update classscanner to work with optional instead of Set
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/ClassScannerTest.java
+++ src/test/java/os/expert/integration/microstream/ClassScannerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 class ClassScannerTest {
 
@@ -17,14 +18,17 @@ class ClassScannerTest {
 
     @Test
     public void shouldReturnEntity() {
-        Set<Class<?>> entities = this.scanner.entities();
+        Set<Class<?>> entities = this.scanner.entity().stream()
+                .collect(Collectors.toUnmodifiableSet());
         Assertions.assertThat(entities).hasSize(1)
                 .contains(Book.class);
     }
 
     @Test
     public void shouldReturnRepository() {
-        Set<Class<?>> repositories = this.scanner.repositories();
+        Set<Class<?>> repositories = this.scanner.repository()
+                .stream()
+                .collect(Collectors.toUnmodifiableSet());
         Assertions.assertThat(repositories).hasSize(1)
                 .contains(Library.class);
     }
