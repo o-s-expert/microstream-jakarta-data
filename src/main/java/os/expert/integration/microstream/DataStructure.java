a6dec28a30df4608eb90767f4c6845c72baa1b4c
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-08 09:42:46 +0000

    doc: increase documentations on the DataStructure methods
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/integration/microstream/DataStructure.java
+++ src/main/java/os/expert/integration/microstream/DataStructure.java
@@ -2,6 +2,7 @@ package os.expert.integration.microstream;
 
 import one.microstream.collections.lazy.LazyHashMap;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,30 +20,69 @@ public class DataStructure {
     private final Map<Object, Object> data = new LazyHashMap();
 
 
+    /**
+     * Associates the specified value with the specified key in this map.
+     *
+     * @param key   the key
+     * @param value the entity
+     * @param <K>   the key type
+     * @param <V>   the entity type
+     */
     public <K, V> void put(K key, V value) {
         Objects.requireNonNull(key, "key is required");
         Objects.requireNonNull(value, "value is required");
         this.data.put(key, value);
     }
 
+    /**
+     * * Returns the value to which the specified key is mapped,
+     * or {@code Optional#empty()} if this map contains no mapping for the key.
+     *
+     * @param key the key or ID
+     * @param <K> the key type
+     * @param <V> the entity type
+     * @return the entity of {@link Optional#empty()}
+     */
     public <K, V> Optional<V> get(K key) {
         Objects.requireNonNull(key, "key is required");
         return (Optional<V>) Optional.ofNullable(this.data.get(key));
     }
 
+    /**
+     * Removes the mapping for a key from this map if it is present
+     *
+     * @param key the key
+     * @param <K> the key type
+     */
     public <K> void remove(K key) {
         Objects.requireNonNull(key, "key is required");
         this.data.remove(key);
     }
 
+    /**
+     * Returns the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map
+     */
     public int size() {
         return this.data.size();
     }
 
+    /**
+     * Returns {@code true} if this map contains no key-value mappings.
+     *
+     * @return {@code true} if this map contains no key-value mappings
+     */
     public boolean isEmpty() {
         return this.data.isEmpty();
     }
 
+    /**
+     * Returns a {@link Collection} view of the values contained in this map.
+     *
+     * @param <V> the entity type
+     * @return a collection view of the values contained in this map
+     */
     public <V> Stream<V> values() {
         return (Stream<V>) this.data.values().stream();
     }
