901833abec05f48012a7fd90611323c455a406ee
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-08 08:48:11 +0000

    feat: add validation on ClassScanner to not support more than one repository and entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/os/expert/integration/microstream/ClassScanner.java
+++ src/main/java/os/expert/integration/microstream/ClassScanner.java
@@ -3,18 +3,16 @@ package os.expert.integration.microstream;
 
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ScanResult;
+import jakarta.data.exceptions.MappingException;
 import jakarta.data.repository.DataRepository;
 import jakarta.data.repository.Repository;
 import jakarta.nosql.Entity;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Logger;
 
 import static java.util.Collections.unmodifiableSet;
-import static java.util.stream.Collectors.toUnmodifiableSet;
 
 /**
  * Scanner classes that will load entities with both Entity and Embeddable
@@ -25,24 +23,31 @@ public enum ClassScanner {
 
     INSTANCE;
 
-    private final Set<Class<?>> entities;
-    private final Set<Class<?>> repositories;
+    private final Class<?> entity;
+    private final Class<?> repository;
 
 
     ClassScanner() {
-        entities = new HashSet<>();
-        repositories = new HashSet<>();
+        Set<Class<?>> entities = new HashSet<>();
+        Set<Class<?>> repositories = new HashSet<>();
 
         Logger logger = Logger.getLogger(ClassScanner.class.getName());
         logger.fine("Starting scan class to find entities, embeddable and repositories.");
         try (ScanResult result = new ClassGraph().enableAllInfo().scan()) {
-            this.entities.addAll(result.getClassesWithAnnotation(Entity.class).loadClasses());
-            this.repositories.addAll(result.getClassesWithAnnotation(Repository.class)
+            entities.addAll(result.getClassesWithAnnotation(Entity.class).loadClasses());
+            repositories.addAll(result.getClassesWithAnnotation(Repository.class)
                     .getInterfaces().loadClasses(DataRepository.class));
         }
         logger.fine(String.format("Finished the class scan with entities %d, and repositories: %d"
                 , entities.size(), repositories.size()));
 
+        if (entities.size() > 1) {
+            throw new MappingException("Microstream Jakarta Data does not support more than one entity using jakarta.nosql.Entity");
+        }
+
+        if (repositories.size() > 1) {
+            throw new MappingException("Microstream Jakarta Data does not support more than one Repository");
+        }
     }
 
 
@@ -64,16 +69,4 @@ public enum ClassScanner {
         return unmodifiableSet(repositories);
     }
 
-    /**
-     * Returns repositories {@link Class#isAssignableFrom(Class)} the parameter
-     *
-     * @param filter the repository filter
-     * @return the list
-     */
-    public Set<Class<?>> repositories(Class<? extends DataRepository> filter) {
-        Objects.requireNonNull(filter, "filter is required");
-        return repositories.stream().filter(filter::isAssignableFrom)
-                .filter(c -> Arrays.asList(c.getInterfaces()).contains(filter))
-                .collect(toUnmodifiableSet());
-    }
 }
