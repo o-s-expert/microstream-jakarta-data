49ecc9aa209ebb72005eb5d7d7fce5b1187d54ca
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-09 15:48:53 +0000

    test: create save operation at Microstream repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/MicrostreamRepositoryBasicOperationTest.java
+++ src/test/java/os/expert/integration/microstream/MicrostreamRepositoryBasicOperationTest.java
@@ -1,9 +1,15 @@
 package os.expert.integration.microstream;
 
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("The Microstream's PageableRepository default methods")
 public class MicrostreamRepositoryBasicOperationTest {
@@ -25,8 +31,28 @@ public class MicrostreamRepositoryBasicOperationTest {
         this.library =  new MicrostreamRepository<>(template);
     }
 
-    @Test
-    public void shouldSave() {
-
+    @ParameterizedTest
+    @ArgumentsSource(BookArgumentProvider.class)
+    public void shouldSaveWhenDataDoesNotExist(Book book) {
+        assertThat(data.isEmpty()).isTrue();
+        this.library.save(book);
+        assertThat(data.isEmpty()).isFalse();
     }
+
+    @Test
+    public void shouldReturnErrorWhenSaveIsNull() {
+        assertThrows(NullPointerException.class, () -> this.library.save(null));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(BookArgumentProvider.class)
+    public void shouldSaveWhenDataExist(Book book) {
+        assertThat(data.isEmpty()).isTrue();
+        this.library.save(book);
+        this.library.save(book);
+        assertThat(data.isEmpty()).isFalse();
+        assertThat(data.size()).isEqualTo(1);
+    }
+
+
 }
