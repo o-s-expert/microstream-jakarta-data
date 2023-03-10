89c1cb5eb23328eb251647acdc841ed64a17caef
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-10 10:11:59 +0000

    test: create structure to to method query
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/test/java/os/expert/integration/microstream/RepositoryMethodQueryTest.java
@@ -0,0 +1,8 @@
+package os.expert.integration.microstream;
+
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("The Microstream's PageableRepository query by methods features")
+public class RepositoryMethodQueryTest {
+}
