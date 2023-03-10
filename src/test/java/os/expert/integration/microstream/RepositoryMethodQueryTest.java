42bcd25f974a30b39dcd7c873f3c54d911a42874
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-10 10:16:57 +0000

    update method by query testl
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/RepositoryMethodQueryTest.java
+++ src/test/java/os/expert/integration/microstream/RepositoryMethodQueryTest.java
@@ -1,8 +1,25 @@
 package os.expert.integration.microstream;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("The Microstream's PageableRepository query by methods features")
 public class RepositoryMethodQueryTest {
+
+    private Library library;
+    private DataStructure data;
+
+    private MicrostreamTemplate template;
+
+    private EntityMetadata metadata;
+
+
+    @BeforeEach
+    public void setUp() {
+        this.metadata = EntityMetadata.of(Book.class);
+        this.data = new DataStructure();
+        this.template = new MicrostreamTemplate(data, metadata);
+        this.library = RepositoryProxySupplier.INSTANCE.get(Library.class, template);
+    }
 }
