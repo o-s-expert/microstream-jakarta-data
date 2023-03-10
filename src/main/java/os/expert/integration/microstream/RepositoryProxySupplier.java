1fd433749a0bd3244a468b41685d6a16674e5cf5
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-10 09:55:50 +0000

    feat: create supplier of proxy
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/main/java/os/expert/integration/microstream/RepositoryProxySupplier.java
@@ -0,0 +1,27 @@
+package os.expert.integration.microstream;
+
+import jakarta.data.repository.PageableRepository;
+
+import java.lang.reflect.Proxy;
+
+class RepositoryProxySupplier {
+
+
+    /**
+     * Produces a Repository class from repository class and {@link MicrostreamTemplate}
+     *
+     * @param repositoryClass the repository class
+     * @param template        the template
+     * @param <T>             the entity of repository
+     * @param <K>             the K of the entity
+     * @param <R>             the repository type
+     * @return a Repository interface
+     */
+    <T, K, R extends PageableRepository<T, K>> R get(Class<R> repositoryClass, MicrostreamTemplate template) {
+        MicrostreamRepository<T, K> repository = new MicrostreamRepository<>(template);
+        RepositoryProxy<T, K> handler = new RepositoryProxy<>(repository);
+        return (R) Proxy.newProxyInstance(repositoryClass.getClassLoader(),
+                (Class<?>[]) new Class[]{repositoryClass},
+                handler);
+    }
+}
