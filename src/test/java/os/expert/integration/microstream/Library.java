dbd621052e361ed62ef90b74c735e360d9d84802
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-10 10:16:38 +0000

    test: add methods on Library
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/Library.java
+++ src/test/java/os/expert/integration/microstream/Library.java
@@ -3,7 +3,26 @@ package os.expert.integration.microstream;
 import jakarta.data.repository.PageableRepository;
 import jakarta.data.repository.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface Library  extends PageableRepository<Book, String> {
+
+    List<Book> findByTitle(String title);
+
+    List<Book> findByTitleOrderByIsbn(String title);
+
+    List<Book> findByEditionLessThan(Integer edition);
+
+    List<Book> findByEditionLessThanEqual(Integer edition);
+
+    List<Book> findByEditionGreaterThan(Integer edition);
+
+    List<Book> findByEditionGreaterThanEqual(Integer edition);
+
+    List<Book> findByEditionIn(Integer edition);
+
+    List<Book> findByEditionBetween(Integer edition, Integer editionB);
 }
+
