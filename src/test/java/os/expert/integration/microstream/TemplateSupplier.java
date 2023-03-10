05e938d60534bb89b069733405112661be60a5e3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-10 22:04:26 +0000

    feat: create Template supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ src/test/java/os/expert/integration/microstream/TemplateSupplier.java
@@ -0,0 +1,39 @@
+package os.expert.integration.microstream;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.nosql.Template;
+import one.microstream.storage.types.StorageManager;
+
+import java.util.function.Supplier;
+
+@ApplicationScoped
+class TemplateSupplier implements Supplier<Template> {
+
+    @Inject
+    private StorageManager manager;
+
+    @Inject
+    private EntityMetadata metadata;
+
+    @Override
+    @Produces
+    @ApplicationScoped
+    public Template get() {
+
+        Object root = manager.root();
+        DataStructure data;
+        if (root == null) {
+            data = new DataStructure();
+            manager.setRoot(data);
+        } else if (root instanceof DataStructure dataStructure) {
+            data = dataStructure;
+        } else {
+            throw new IllegalArgumentException("The current root structure is incompatible with DataStructure. " +
+                    "The current structure class: " + root.getClass());
+        }
+        return new MicrostreamTemplate(data, metadata);
+    }
+}
