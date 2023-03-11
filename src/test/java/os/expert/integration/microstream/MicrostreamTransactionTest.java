9dd4954e52ceab052c3343540c6bd13bef2c19f9
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-11 03:22:10 +0000

    test: create scenario to save with library
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/os/expert/integration/microstream/MicrostreamTransactionTest.java
+++ src/test/java/os/expert/integration/microstream/MicrostreamTransactionTest.java
@@ -9,6 +9,8 @@ import org.jboss.weld.junit5.auto.EnableAutoWeld;
 import org.junit.jupiter.api.Test;
 
 import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableAutoWeld
 @AddPackages(value = ReturnType.class)
@@ -19,11 +21,30 @@ public class MicrostreamTransactionTest {
     @Inject
     private Template template;
 
+    @Inject
+    private Library library;
 
     @Test
     public void shouldInsert() {
         Book book = Book.builder().isbn("1231").title("Clean Code").author("Robert Martin")
                 .edition(1).release(Year.of(2020)).build();
         this.template.insert(book);
+    }
+
+    @Test
+    public void shouldInsertList() {
+        List<Book> books = new ArrayList<>();
+        Book book = Book.builder().isbn("1231").title("Clean Code").author("Robert Martin")
+                .edition(1).release(Year.of(2020)).build();
+        books.add(book);
+        books.add(book);
+        this.template.insert(books);
+    }
+
+    @Test
+    public void shouldSave() {
+        Book book = Book.builder().isbn("1231").title("Clean Code").author("Robert Martin")
+                .edition(1).release(Year.of(2020)).build();
+        this.library.save(book);
     }
 }
