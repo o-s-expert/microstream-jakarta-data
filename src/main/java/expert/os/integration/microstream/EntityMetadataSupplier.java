a195b264d5a50f6a6b5997c7fcdef970145ee4b6
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-14 09:57:20 +0100

    feat: update entity metadata supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/expert/os/integration/microstream/EntityMetadataSupplier.java
+++ src/main/java/expert/os/integration/microstream/EntityMetadataSupplier.java
@@ -15,21 +15,18 @@
 
 package expert.os.integration.microstream;
 
-import jakarta.data.exceptions.MappingException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 
 import java.util.function.Supplier;
 
 @ApplicationScoped
-class EntityMetadataSupplier implements Supplier<EntityMetadata> {
-
+class EntityMetadataSupplier implements Supplier<Entities> {
 
     @Override
     @Produces
-    public EntityMetadata get() {
-        Class<?> entity = ClassScanner.INSTANCE.entity()
-                .orElseThrow(() -> new MappingException("For the integration you need one entity with @Entity annotation"));
-        return EntityMetadata.of(entity);
+    @ApplicationScoped
+    public Entities get() {
+        return Entities.of(ClassScanner.INSTANCE.entities());
     }
 }
