73a221f6fcba031c8f4d535b214f10d19c56040b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-15 13:56:55 +0100

    style: remove unecessary imports on proxy
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/expert/os/integration/microstream/RepositoryProxy.java
+++ src/main/java/expert/os/integration/microstream/RepositoryProxy.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class RepositoryProxy<T, K> implements InvocationHandler {
